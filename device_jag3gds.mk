3f5cd4aa77c10da8c2025176764ff9cbc7a3bcd8
vm03 <vasy@vasy.ru>
2017-01-20 16:36:07 +0300

    fm: updates for brcm fm hal
    Change-Id: Ice3eedad71e0ad0ff7070afb7655c9fef2820f3f

--- device_jag3gds.mk
+++ device_jag3gds.mk
@@ -47,6 +47,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PACKAGES += hwaddrs
 
+# FM
+PRODUCT_PACKAGES += \
+    FMRadio \
+    brcm-uim-sysfs \
+    libfmjni
+
 # Inherit from msm8226-common
 $(call inherit-product, device/lge/msm8226-common/msm8226.mk)
 $(call inherit-product, vendor/lge/jag3gds/jag3gds-vendor.mk)
