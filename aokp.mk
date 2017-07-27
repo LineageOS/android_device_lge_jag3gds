12bb1462f8edb0eed2083ee2491952f285f1136b
Alexandr Kryvenko <wersus99422@gmail.com>
2017-07-27 22:34:12 +0300

    jag3gds: AOKP-ify
    Lineage OS -> AOKP

--- aokp.mk
+++ aokp.mk
@@ -1,5 +1,5 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AOKP stuff.
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/lge/jag3gds/device_jag3gds.mk)
 
@@ -9,7 +9,7 @@ TARGET_SCREEN_HEIGHT := 1280
 
 # Release name
 PRODUCT_RELEASE_NAME := LG G3s
-PRODUCT_NAME := lineage_jag3gds
+PRODUCT_NAME := aokp_jag3gds
 PRODUCT_MODEL := LG-D724
 PRODUCT_DEVICE := jag3gds
 PRODUCT_MANUFACTURER := LGE
